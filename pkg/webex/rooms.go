5fc03e0eb22604896e3c2a46742a230d168ab98a
Danny Rosenthal <dsrosen6@gmail.com>
2025-07-08 15:48:18 -0500

    add get room

--- pkg/webex/rooms.go
+++ pkg/webex/rooms.go
@@ -13,3 +13,12 @@ func (c *Client) ListRooms(ctx context.Context) ([]Room, error) {
 
 	return r.Items, nil
 }
+
+func (c *Client) GetRoom(ctx context.Context, roomID string) (*Room, error) {
+	r := &Room{}
+	if err := c.request(ctx, "GET", fmt.Sprintf("rooms/%s", roomID), nil, r); err != nil {
+		return nil, fmt.Errorf("getting room: %w", err)
+	}
+
+	return r, nil
+}
