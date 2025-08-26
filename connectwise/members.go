28ab1a6114b9b2102e291ac90dc4c78326bd67c0
Danny Rosenthal <dsrosen6@gmail.com>
2025-08-26 11:55:27 -0500

    add GetMembersByIdentifier method

--- connectwise/members.go
+++ connectwise/members.go
@@ -16,6 +16,27 @@ func (c *Client) ListMembers(params map[string]string) ([]Member, error) {
 	return GetMany[Member](c, "system/members", params)
 }
 
+func (c *Client) GetMemberByIdentifier(identifier string) (*Member, error) {
+	p := map[string]string{
+		"conditions": fmt.Sprintf("identifier='%s'", identifier),
+	}
+
+	members, err := c.ListMembers(p)
+	if err != nil {
+		return nil, fmt.Errorf("listing members: %w", err)
+	}
+
+	if len(members) == 0 {
+		return nil, ErrNotFound
+	}
+
+	if len(members) > 1 {
+		return nil, fmt.Errorf("expected 1 member for identifier %s, received %d", identifier, len(members))
+	}
+
+	return &members[0], nil
+}
+
 func (c *Client) GetMember(memberID int, params map[string]string) (*Member, error) {
 	return GetOne[Member](c, memberIdEndpoint(memberID), params)
 }
