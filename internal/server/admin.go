9c6e1823de7cc3240cb5ad2a2a374506c2c49aa2
Danny Rosenthal <dsrosen6@gmail.com>
2025-11-05 11:06:53 -0600

    add users tab

--- internal/server/admin.go
+++ internal/server/admin.go
@@ -9,9 +9,19 @@ import (
 func (cl *Client) handleBaseAdminPage(c *gin.Context) {
 	boards, err := cl.Queries.ListBoards(c.Request.Context())
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		c.Error(err)
 		return
 	}
 
 	c.HTML(http.StatusOK, "base.gohtml", gin.H{"Title": "TicketBot Admin Dashboard", "Boards": boards})
+}
+
+func (cl *Client) handleAdminUsersPage(c *gin.Context) {
+	users, err := cl.Queries.ListUsers(c.Request.Context())
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.HTML(http.StatusOK, "users.gohtml", gin.H{"Title": "Users", "Users": users})
 }
